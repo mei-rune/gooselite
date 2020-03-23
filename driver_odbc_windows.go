518f5511d0c6b188ec81711647464cdffabf8837
meifakun <runner.mei@gmail.com>
2020-03-23 08:23:31 +0800

    update odbc

--- driver_odbc_windows.go
+++ driver_odbc_windows.go
@@ -1,14 +1,13 @@
 package goose
 
 import (
-	//_ "code.google.com/p/odbc"
-	_ "github.com/runner-mei/odbc"
+	_ "github.com/alexbrainman/odbc"
 )
 
 func createMsSqlDriver(name, open string) DBDriver {
 	return DBDriver{Name: "odbc",
 		OpenStr: open,
-		Import:  "github.com/runner-mei/odbc",
+		Import:  "github.com/alexbrainman/odbc",
 		Dialect: DialectByName("mssql")}
 }
 
