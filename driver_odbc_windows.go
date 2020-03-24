3659c30be4e156a5f5d78da2afa391311c1d4995
meifakun <runner.mei@gmail.com>
2020-03-24 12:03:30 +0800

    Revert "update odbc"
    This reverts commit 518f5511d0c6b188ec81711647464cdffabf8837.

--- driver_odbc_windows.go
+++ driver_odbc_windows.go
@@ -1,13 +1,14 @@
 package goose
 
 import (
-	_ "github.com/alexbrainman/odbc"
+	//_ "code.google.com/p/odbc"
+	_ "github.com/runner-mei/odbc"
 )
 
 func createMsSqlDriver(name, open string) DBDriver {
 	return DBDriver{Name: "odbc",
 		OpenStr: open,
-		Import:  "github.com/alexbrainman/odbc",
+		Import:  "github.com/runner-mei/odbc",
 		Dialect: DialectByName("mssql")}
 }
 
